028671a9aac8f63074405eb7444ae90bb526b9e9
pikalucero10 <todointeligenciaartificial99@gmail.com>
2024-07-18 17:10:11 -0300

    b

---
+++ lib/setting.php
@@ -0,0 +1,18 @@
+<?php
+//<!--
+
+// COPYRIGHT © 2022 RECODE SCTV_DIGITAL //
+
+// PHONE 083191910986 //
+
+// DI LARANG KERAS MEMPERJUALBELIKAN SCRIPT INI TANPA IZIN //
+
+// ALL RIGHTS RESERVED //
+
+//-->
+
+//Pengaturan Website
+$config['web'] = array(
+	'url' => 'https://yab-group.com/' // ex: http://domain.com/
+);
+?>
